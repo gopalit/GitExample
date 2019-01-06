9d7bdec7d165cdf0d41ac8408f3558635729ef1a
Gopal <gopalit@yahoo.com>
2019-01-06 00:04:34 -0700

    Add project files.

---
+++ GITExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GITExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hi, This is GIT Example");
+
+            Console.WriteLine("GITCheck-in");
+
+        }
+    }
+}
